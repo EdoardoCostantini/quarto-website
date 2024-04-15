d93cafc9f62805cbf8c80efd97d890e323e4b5b1
Edo <38205413+EdoardoCostantini@users.noreply.github.com>
2024-04-15 17:57:49 +0200

    content: add twitter example data

--- posts/series-programming/python/basics/code/00_basics.py
+++ posts/series-programming/python/basics/code/00_basics.py
@@ -184,6 +184,47 @@ yell = shout("congratulations", "you")
 # Print yell
 print(yell)
 
+# ---------------------------------------------------------------------------- #
+# Example: Twitter data
+# ---------------------------------------------------------------------------- #
+
+# Import Twitter data as DataFrame: df
+tweets_df = pd.read_csv("./posts/series-programming/python/basics/data/tweets.csv")
+
+# Explore the columns
+for col in tweets_df.columns:
+    print(col)
+
+# Unique values of the column
+tweets_df.lang.unique()
+
+# Define a function to count the number of entries in a column of a DataFrame
+def count_entries(df, col_name):
+    """Return a dictionary with counts of
+    occurrences as value for each key."""
+
+    # Initialize an empty dictionary: langs_count
+    langs_count = {}
+
+    # Extract column from DataFrame: col
+    col = df[col_name]
+
+    # Iterate over lang column in DataFrame
+    for entry in col:
+
+        # If the language is in langs_count, add 1
+        if entry in langs_count.keys():
+            langs_count[entry] += 1
+        # Else add the language to langs_count, set the value to 1
+        else:
+            langs_count[entry] = 1
+
+    # Return the langs_count dictionary
+    return langs_count
+
+# Use the function
+count_entries(df = tweets_df, col_name="lang")
+
 ################################################################################
 # Methods
 ################################################################################
